bf4667b02d949df073af314a69c52c58670b7f6c
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-17 18:34:10 +0800

    memasukan int main

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -35,3 +35,11 @@ void mahasiswa::cetak() {
 	cout << endl << "Nim = " << nim << endl;
 	cout << " Nama = " << nama << endl;
 }
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(30, "Fauzan");
+
+	
