a76f9a59e80e77317c36c0bbc685807d89e959d6
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-17 12:16:36 +0800

    memasukan int main

--- staticmember/staticmember.cpp
+++ staticmember/staticmember.cpp
@@ -23,3 +23,11 @@ void mahasiswa::printAll() {
 	cout << "Nama =" << nama << endl;
 	cout << endl;
 }
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko Purbo");
